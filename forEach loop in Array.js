ee95bbe38bf8d9c948b415dba562ffa33ad4e772
praveensinghkuntia77390 <ps7324900@gmail.com>
2024-05-14 20:38:45 +0530

    changes

--- forEach loop in Array.js
+++ forEach loop in Array.js
@@ -3,3 +3,10 @@ let arr=["delhi","pune","hydrabad","banglore"];
  arr.forEach((val,i,arr)=>{
     console.log(val.toUpperCase(),i,arr);
  });
+
+ //Practice Set Question for square
+let nums=[2,3,4,5,6];
+let calcSquare=(square)=>{
+    console.log(square*square);
+};
+nums.forEach(calcSquare);
